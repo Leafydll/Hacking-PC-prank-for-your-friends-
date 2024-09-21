bd090d2d87f56706f2ed4dcde6e6adab1210e3df
Leafydll <elliottvtrounstine@gmail.com>
2024-09-20 20:01:33 -0700

    Add files via upload

---
+++ FakeVirusForYourFriends.bat
@@ -0,0 +1,6 @@
+@echo off 
+:a
+color 02
+title Downloading a BAT file? Bruhhhhhhhh
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto a 
